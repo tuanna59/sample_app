3cc6a8a997c8454140ba786a64aab90e8093c0b7
Tuan Nguyen <nguyenanhtuan090596@gmail.com>
2017-11-15 11:04:23 +0700

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171113231202) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,4 +20,5 @@ ActiveRecord::Schema.define(version: 20171113231202) do
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
 end
